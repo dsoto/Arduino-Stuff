13fa91042a2a448e2c5b69aa46567ef180da7418
Daniel Soto <danielrsoto@gmail.com>
2010-09-27 19:49:28 -0400

    compiles and flashes at correct rate with help of init()

--- arduino-gcc-blink/blink.c
+++ arduino-gcc-blink/blink.c
@@ -1,10 +1,13 @@
-#include "Wiring.h"
+#include "WProgram.h"
 void main(){
-  pinMode(13, OUTPUT);
+  // this init() is part of the arduino setup
+  init();
+  pinMode(8, OUTPUT);
+
 while(1){
-  digitalWrite(13, HIGH);   // set the LED on
+  digitalWrite(8, HIGH);   // set the LED on
   delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
+  digitalWrite(8, LOW);    // set the LED off
   delay(1000);              // wait for a second
  }
 }
