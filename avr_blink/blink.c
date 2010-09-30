52035e054eb6e426e73a1b0ca2c0324fd472d6e1
Daniel Soto <danielrsoto@gmail.com>
2010-09-30 19:54:22 -0400

    avr-blink - now listens to pin as interrupt and then initiates blink

--- avr_blink/blink.c
+++ avr_blink/blink.c
@@ -2,25 +2,38 @@
 // this clock speed is the 9.6MHz divided by the 8 clock prescaler
 #define F_CPU 1200000UL
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main (void)
-{
-    DDRB  = 0xff;
-    // initialize bit high
-    PORTB |= _BV(PORTB3);
+int i;
 
-    while(1)
+int main ()
+{
+    // pin 3 output
+    DDRB |= _BV(DDB3);
+    // initialize bit low
+    PORTB &= ~_BV(PORTB3);
+
+    // pin 4 input
+    DDRB &= ~_BV(DDB4);
+    // turn on pullup resistor to config as switch
+    PORTB |= _BV(PORTB4);
+
+    // initialize interrupts
+    sei();
+    GIMSK |= _BV(PCIE);
+    PCMSK |= _BV(PCINT4);
+
+    // loop while interrupts run the show
+    while(1);
+}
+
+ISR (PCINT0_vect) {
+    for (i=1;i<20;i++)
     {
-		//PORTB = 0xff;
 		// toggle pin (^) is the XOR operator
-		PORTB ^= _BV(PORTB3);
-        _delay_ms(500);
-		//PORTB = 0x00;
-		// toggle pin agin
 		PORTB ^= _BV(PORTB3);
         _delay_ms(50);
     }
 }
-
